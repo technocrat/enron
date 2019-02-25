50dacd86332daa7841933a0ecdb27ed38039059f
Richard Careaga <public@careaga.net>
2019-02-25 13:48:25 -0800

    Checkpoint for system conversion

--- tb.R
+++ tb.R
@@ -237,8 +237,9 @@ centrals <- prominence$node
 # Sun Feb 24 22:29:51 2019 ------------------------------
 # g_enron filted for highest 16 users based on centrality
 #
-net_p <- network(prominence, matrix.type = "edgelist")
+net_p <- network(incrowd, matrix.type = "edgelist")
 vertices <- network.vertex.names(net_p)
+
 netmat <- g_enron %>% filter(f_userid == 1648 | t_userid == 1648)
 net1648 <- network(netmat, matrix.type = "edgelist")
 vertices <- network.vertex.names(net1648)
@@ -250,82 +251,17 @@ egomap <- function(x) {
   vertices <- network.vertex.names(e_net)
   ggnet(e_net, size = 0.1, alpha = 0.84)
 }
-library(tm)
-library(here)
-uri <- here("wolak.pdf")
-engine <- "pdftools"
-reader <- readPDF(engine)
-wolak <- reader(elem = list(uri = uri), language = "en", id = "id1")
-save(wolak, file = "wolak.Rda")
-wolak <- VectorSource(wolak)
-w_corpus <- VCorpus(wolak)
-w_corpus <- tm_map(w_corpus, stripWhitespace)
-w_corpus <- tm_map(w_corpus, removeWords, stopwords("english"))
-w_corpus <- tm_map(w_corpus, removeNumbers)
-w_corpus <- tm_map(w_corpus, removePunctuation, preserve_intra_word_contractions = TRUE, preserve_intra_word_dashes = TRUE)
-w_corpus <- tm_map(w_corpus, stemDocument)
-save(w_corpus, file = "w_corpus.Rda")
-w_dtm <- DocumentTermMatrix(w_corpus)
-removeSparseTerms(w_dtm, 0.2)
-save(w_dtm, file = "w_dtm.Rda")
-#findFreqTerms(w_dtm, 5)
-#findAssocs(w_dtm, "day-ahead", 0.8)
-w_ctm <- CTM(w_dtm, k = 5) # how to unpack
-w_lda <- LDA(w_dtm, control = list(alpha = 0.1), k = 5)
-w_lda_inf <- posterior(w_lda, w_dtm)
 
-term_tfidf <-
-  + tapply(w_dtm$v/row_sums(w_dtm)[w_dtm$i], w_dtm$j, mean) *
-  + log2(nDocs(w_dtm)/col_sums(w_dtm > 0))
+prominent <- g_enron %>% filter(f_userid %in% centrals | t_userid %in% centrals)
+senders <- prominent %>% filter(f_userid %in% centrals) %>% group_by(f_userid) %>% count() %>% arrange(desc(n))
 
-w_dtm <- w_dtm[,term_tfidf >= 0.1]
-w_dtm <- w_dtm[row_sums(w_dtm) > 0,]
-summary(col_sums(w_dtm))
+recipients <- prominent %>% filter(t_userid %in% centrals) %>% group_by(t_userid) %>% count() %>% arrange(desc(n))
 
-library("topicmodels")
-k <- 3
-SEED <- 2010
-w_TM <- list(VEM = LDA(w_dtm, k = k, control = list(seed = SEED)),VEM_fixed = LDA(w_dtm, k = k,control = list(estimate.alpha = FALSE, seed = SEED)), Gibbs = LDA(w_dtm, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000)), CTM = CTM(w_dtm, k = k, control = list(seed = SEED, var = list(tol = 10^-4), em = list(tol = 10^-3))))
+inout <- bind_cols(sender, recipients)
 
-sapply(w_TM[1:2], slot, "alpha")
-
-w_Topic <- topics(w_TM[["VEM"]], 1)
-w_terms <- terms(w_TM[["VEM"]], 250)
-Terms[,1:5]
-
-w_terms <- tidy(w_dtm)
-w_terms <- w_terms %>% mutate(term = str_replace(term, "^[:punct:]", ""))
-
-# Sat Feb 23 17:05:05 2019 ------------------------------
-# can be no blank rows in dtm, but nrow must be kept in sync
-# Must also re run networks -----------------------------
-#g_enron <- g_enron %>% filter(nchar(payload) <10) # filters 692 blanks
-g_bag <- g_enron %>% select(payload)
-g <- VectorSource(g_bag)
-g_corpus <- tm_map(g_corpus, stripWhitespace)
-g_corpus <- tm_map(g_corpus, removeWords, stopwords("english"))
-g_corpus <- tm_map(g_corpus, removeNumbers)
-g_corpus <- tm_map(g_corpus, removePunctuation, preserve_intra_word_contractions = TRUE, preserve_intra_word_dashes = TRUE)
-g_corpus <- tm_map(g_corpus, stemDocument)
-#save(g_corpus, file = "g_corpus.Rda")
-g_dtm <- DocumentTermMatrix(g_corpus)
-# Sat Feb 23 15:12:14 2019 ------------------------------
-# Remove all zero rows
-rowTotals <- apply(g_dtm , 1, sum)
-g_dtm <- g_dtm[rowTotals> 0, ]
-
-removeSparseTerms(g_dtm, 0.2)
-save(g_dtm, file = "g_dtm.Rda")
-
-library("topicmodels")
-
-
-
-k <- 3
-SEED <- 2010
-g_TM <- list(VEM = LDA(g_dtm, k = k, control = list(seed = SEED)),VEM_fixed = LDA(g_dtm, k = k,control = list(estimate.alpha = FALSE, seed = SEED)), Gibbs = LDA(g_dtm, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000)), CTM = CTM(g_dtm, k = k, control = list(seed = SEED, var = list(tol = 10^-4), em = list(tol = 10^-3))))
-
-sapply(g_TM[1:2], slot, "alpha")
-
-
-g_terms<- terms(g_TM[["VEM"]], 250)
+netmat <- incrowd %>% select(t_userid, f_userid)
+net_p <- network(netmat, matrix.type = "edgelist")
+vertices <- network.vertex.names(net_p)
+ggnet(net_p, size = 0.1, alpha = 0.84)
+delete.vertices(net_p, isolates(net_p))
+ggnet(net_p, size = 0.1, alpha = 0.84)
