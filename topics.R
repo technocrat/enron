4436e0163c53bd098778efa68d9205583c317545
Richard Careaga <public@careaga.net>
2019-03-09 20:52:20 -0700

    Setup LDA section, attempt to retrace earlier analysis

--- topics.R
+++ topics.R
@@ -2,15 +2,17 @@ setwd("projects/enron")
 library(broom)
 library(GGally)
 library(here)
+library(slam)
+library(tidyr)
 library(tidytext)
 library(tidyverse)
 library(tm)
 library(topicmodels)
-uri <- here("wolak.pdf")
+uri <- here("sources/wolak.pdf")
 engine <- "pdftools"
 reader <- readPDF(engine)
 wolak <- reader(elem = list(uri = uri), language = "en", id = "id1")
-save(wolak, file = "wolak.Rda")
+#save(wolak, file = "wolak.Rda")
 wolak <- VectorSource(wolak)
 w_corpus <- VCorpus(wolak)
 w_corpus <- tm_map(w_corpus, stripWhitespace)
@@ -18,10 +20,10 @@ w_corpus <- tm_map(w_corpus, removeWords, stopwords("english"))
 w_corpus <- tm_map(w_corpus, removeNumbers)
 w_corpus <- tm_map(w_corpus, removePunctuation, preserve_intra_word_contractions = TRUE, preserve_intra_word_dashes = TRUE)
 w_corpus <- tm_map(w_corpus, stemDocument)
-save(w_corpus, file = "w_corpus.Rda")
+#save(w_corpus, file = "w_corpus.Rda")
 w_dtm <- DocumentTermMatrix(w_corpus)
 removeSparseTerms(w_dtm, 0.2)
-save(w_dtm, file = "w_dtm.Rda")
+# save(w_dtm, file = "w_dtm.Rda")
 #findFreqTerms(w_dtm, 5)
 #findAssocs(w_dtm, "day-ahead", 0.8)
 w_ctm <- CTM(w_dtm, k = 5) # how to unpack
@@ -36,7 +38,6 @@ w_dtm <- w_dtm[,term_tfidf >= 0.1]
 w_dtm <- w_dtm[row_sums(w_dtm) > 0,]
 summary(col_sums(w_dtm))
 
-library("topicmodels")
 k <- 3
 SEED <- 2010
 w_TM <- list(VEM = LDA(w_dtm, k = k, control = list(seed = SEED)),VEM_fixed = LDA(w_dtm, k = k,control = list(estimate.alpha = FALSE, seed = SEED)), Gibbs = LDA(w_dtm, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000)), CTM = CTM(w_dtm, k = k, control = list(seed = SEED, var = list(tol = 10^-4), em = list(tol = 10^-3))))
@@ -69,9 +70,7 @@ rowTotals <- apply(g_dtm , 1, sum)
 g_dtm <- g_dtm[rowTotals> 0, ]
 
 removeSparseTerms(g_dtm, 0.2)
-save(g_dtm, file = "g_dtm.Rda")
-
-library("topicmodels")
+#save(g_dtm, file = "g_dtm.Rda")
 
 
 
